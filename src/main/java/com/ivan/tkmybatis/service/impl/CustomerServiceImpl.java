debb81619dbee9dd9c4c47f5b691090a877cf982
Ivanyany <754339948@qq.com>
2020-03-26 19:23:55 +0800

    其他方法

--- src/main/java/com/ivan/tkmybatis/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/ivan/tkmybatis/service/impl/CustomerServiceImpl.java
@@ -20,12 +20,67 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     CustomerMapper customerMapper;
 
+    /**
+     * @Author Ivan
+     * @Description 利用tk.mybatis查询
+     * @Date 2020/3/26 19:15
+     * @Param
+     * @return
+     */
     public List<Customer> selectAllCustomer(){
         return customerMapper.selectAll();
     }
 
+    /**
+     * @Author Ivan
+     * @Description 自定义查询
+     * @Date 2020/3/26 19:16
+     * @Param
+     * @return
+     */
     @Override
     public Customer getById(Long id) {
         return customerMapper.selectById(id);
+    }
+
+    public void otherMethod(){
+        //插入一条数据,所有的字段都会添加一遍,即使没有值
+//        customerMapper.insert(T);
+
+        //插入一条数据,只给有值的字段赋值（会对传进来的值做非空判断）
+//        customerMapper.insertSelective(T);
+
+        //*****************************************************
+
+        //根据对象删除数据
+//        customerMapper.delete(T)
+
+        //根据主键删除数据
+//        customerMapper.deleteByPrimaryKey(id);
+
+        //*****************************************************
+
+        //根据主键修改数据,所有字段都修改
+//        customerMapper.updateByPrimaryKey(T);
+
+        //根据主键修改数据,只修改有值得字段
+//        customerMapper.updateByPrimaryKeySelective(T);
+
+        //*****************************************************
+
+        //根据对象查询
+//        customerMapper.select(T);
+
+        //查询所有
+//        customerMapper.selectAll();
+
+        //根据主键查询
+//        customerMapper.selectByPrimaryKey(id);
+
+        //查询数据个数
+//        customerMapper.selectCount(T);
+
+        //查询一条数据
+//        customerMapper.selectOne(T);
     }
 }
